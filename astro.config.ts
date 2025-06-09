53607d84c224d74081e46c1e48e229359dad0824
Catalina Escalona <catalinaescalona@gmail.com>
2025-06-09 14:38:41 +0200

    removed adapter to use proper github actions deploy

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,8 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: 'https://catalinaescalona.github.io',
+  base: "/", // Ensure this is correct if your site is served at root
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
@@ -16,7 +17,6 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
       themes: { light: "min-light", dark: "night-owl" },
       wrap: true,
     },
@@ -28,8 +28,6 @@ export default defineConfig({
     },
   },
   image: {
-    // Used for all Markdown images; not configurable per-image
-    // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
     experimentalLayout: "constrained",
   },
   env: {
