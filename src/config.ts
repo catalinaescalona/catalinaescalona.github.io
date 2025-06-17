7ffdba830de12b75ca49506f5f8dcd0e98d5266c
Catalina Escalona <catalinaescalona@gmail.com>
2025-06-18 00:31:44 +0200

    updated intro on index page

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export const SITE = {
   postPerIndex: 10,
   postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-  showArchives: true,
+  showArchives: false,
   showBackButton: true, // show back button in post detail
   editPost: {
     enabled: false,
