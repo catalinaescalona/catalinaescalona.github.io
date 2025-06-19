eec389d62c109c616577f1e8be8dfbd728468f71
Catalina Escalona <catalinaescalona@gmail.com>
2025-06-20 00:45:32 +0200

    explicitly slice for post numbers

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export const SITE = {
   title: "Catalina Escalona",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 10,
-  postPerPage: 10,
+  postPerIndex: 12,
+  postPerPage: 12,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: false,
   showBackButton: true, // show back button in post detail
